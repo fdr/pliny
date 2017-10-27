0fdbe19cfe5241030344d28fe7fb6ce7f800af12
Daniel Farina <daniel@fdr.io>
2017-10-27 16:44:58 -0700

    diagnostics

--- lib/pliny/helpers/encode.rb
+++ lib/pliny/helpers/encode.rb
@@ -2,7 +2,10 @@ module Pliny::Helpers
   module Encode
     def encode(object)
       content_type :json, charset: 'utf-8'
-      MultiJson.encode(object, pretty: params[:pretty] == 'true' || Config.pretty_json)
+      p object
+      out = MultiJson.encode(object, pretty: params[:pretty] == 'true' || Config.pretty_json)
+      p out
+      out
     end
   end
 end
