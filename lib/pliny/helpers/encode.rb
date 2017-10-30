e14da2284d25711ceecc4873f64ff8992618593d
Daniel Farina <daniel@fdr.io>
2017-10-30 13:19:20 -0700

    okay

--- lib/pliny/helpers/encode.rb
+++ lib/pliny/helpers/encode.rb
@@ -3,8 +3,8 @@ module Pliny::Helpers
     def encode(object)
       content_type :json, charset: 'utf-8'
       p object
-      out = Oj.dump(object, indent: 2)
-      # out = MultiJson.encode(object, pretty: params[:pretty] == 'true' || Config.pretty_json)
+      # out = Oj.dump(out, indent: 2)
+      out = MultiJson.encode(object) #, pretty: params[:pretty] == 'true' || Config.pretty_json)
       p ['asdf encoded', out]
       out
     end
